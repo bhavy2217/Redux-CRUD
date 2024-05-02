f54c40cea34a4cc66b16312db3f8c0064f891452
Bhavy Panchal <78581459+bhavy2217@users.noreply.github.com>
2024-05-02 19:34:30 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name : "sahil patel",
+        email:'s@gmail.com',
+        id:1
+    },
+    {
+        name : "Akash Patil",
+        email:'a@gmail.com',
+        id:2,
+    },
+    {
+        name : "MS Dhoni",
+        email:'ms@gmail.com',
+        id:3,
+    },
+]
